dff2296fc020e86a2dec0b9fc982592a3906840a
zekikaya1 <zzeki.kaya@gmail.com>
2025-02-21 23:22:33 +0300

    feat(TASK-12): UseSerilog and kibana

--- src/Logiwa.Product.Api/Program.cs
+++ src/Logiwa.Product.Api/Program.cs
@@ -1,13 +1,19 @@
 using Logiwa.Product.Api;
 using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
 
 Log.Logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(builder.Configuration) 
-    .Enrich.FromLogContext()
-    .WriteTo.Console()
+    .MinimumLevel.Debug()
+    .WriteTo.Console() 
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://elasticsearch:9200"))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = "logiwa-{0:yyyy.MM.dd}"
+    })
     .CreateLogger();
+
 
 builder.Host.UseSerilog();
 
